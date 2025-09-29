22f7c17acdad97e4d36faa7152ae65e44b8a513b
Lovepreet Singh <lovepreetsinghvirk5781@gmail.com>
2025-09-29 08:13:54 +0530

    the build1
    Former-commit-id: 75fca00263bc3cc29b53f1d53c0644df03ec1ce0

---
+++ android/app/build/intermediates/cxx/Debug/5a705h46/logs/armeabi-v7a/clean_command.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\User\\Soft\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\rn\\thenewone\\android\\app\\.cxx\\Debug\\5a705h46\\armeabi-v7a" ^
+  clean
---
+++ android/app/build/intermediates/cxx/RelWithDebInfo/5hr4y55p/logs/armeabi-v7a/build_command_targets.bat
@@ -0,0 +1,6 @@
+@echo off
+"D:\\User\\Soft\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\rn\\thenewone\\android\\app\\.cxx\\RelWithDebInfo\\5hr4y55p\\armeabi-v7a" ^
+  appmodules ^
+  react_codegen_safeareacontext
