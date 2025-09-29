110b865383114838e4ab4168d49ea2ab297d8cc3
Lovepreet Singh <lovepreetsinghvirk5781@gmail.com>
2025-09-29 08:19:58 +0530

    Remove Android build files
    Former-commit-id: 481a6da714c2d3044c488c8a9153254248525978

--- android/app/build/intermediates/cxx/Debug/5a705h46/logs/armeabi-v7a/clean_command.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\User\\Soft\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\rn\\thenewone\\android\\app\\.cxx\\Debug\\5a705h46\\armeabi-v7a" ^
-  clean
--- android/app/build/intermediates/cxx/RelWithDebInfo/5hr4y55p/logs/x86/build_command_targets.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-"D:\\User\\Soft\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\rn\\thenewone\\android\\app\\.cxx\\RelWithDebInfo\\5hr4y55p\\x86" ^
-  appmodules ^
-  react_codegen_safeareacontext
